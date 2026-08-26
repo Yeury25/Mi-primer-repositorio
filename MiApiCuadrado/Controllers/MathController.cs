c5e038f8a9f1e9cb2b85e8844ee09789219a81e5
Yeury25 <yeuricastillo25@gmail.com>
2026-08-26 13:04:34 -0400

    Agrega controlador para cuadrado

---
+++ MiApiCuadrado/Controllers/MathController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiApiCuadrado.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MathController : ControllerBase
+{
+    [HttpGet("cuadrado/{numero:int}")]
+    public IActionResult Cuadrado(int numero)
+    {
+        if (numero < 0)
+            return BadRequest("El número debe ser mayor o igual a 0.");
+
+        return Ok(numero * numero);
+    }
+}
